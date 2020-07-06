c8c2a1cfff91cac3dc8bb667a0b4265cc8156d1a
mrsaicharan1 <saicharan.reddy1@gmail.com>
2020-07-06 19:14:15 +0530

    Added unit tests for worker methods
    Signed-off-by: mrsaicharan1 <saicharan.reddy1@gmail.com>

---
+++ tests/test_helpers.py
@@ -0,0 +1,28 @@
+import pytest
+import pandas as pd
+from queue import Queue
+from workers.util import read_config
+from workers.worker_base import Worker
+
+
+def test_check_duplicates():
+    obj = {"website":["walmart.com"]}
+    new_data = [obj]
+    table_values = pd.DataFrame.from_dict({'website': ['walmart.com','augurlabs.io']})
+    assert Worker.check_duplicates(new_data, table_values, "website") == [obj]
+
+def test_dump_queues():
+    sample_queue = Queue()
+    list_sample = ["x@x.com", "y@y.com", "z@z.com"]
+    for list_item in list_sample:
+        sample_queue.put(list_item)
+    queue_to_list = Worker.dump_queue(sample_queue)
+    assert queue_to_list == ["x@x.com", "y@y.com", "z@z.com"]
+
+def test_read_config_no_exception():
+    db_name = read_config('Database', 'user', 'AUGUR_DB_USER', 'augur', config_file_path="augur.config.json")
+    assert db_name == "augur"
+
+def test_read_config_exception():
+    with pytest.raises(AttributeError):
+        db_name = read_config('Server', 'username')
